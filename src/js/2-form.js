1680cb29861f9f7c5684376772ffada08e02a526
Dima2430 <dima.volkkom@gmail.com>
2024-01-27 19:25:06 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,7 +1,19 @@
-function saveToLS(key="empty",value="") {
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', (e) => {
+    const userEmail = form.elements.email.value;
+    const userText = form.elements.text.value;
+    const data = {
+        email: userEmail,
+        message: userText,
+    };
+    saveToLS(STORAGE_KEY, data);
+});
+
+function saveToLS(key = "empty", value = "") {
     const data = JSON.stringify(value);
     localStorage.setItem(key,data)
-}
+};
 function loadFromLS(key="empty") {
     const data = localStorage.getItem(key);
     try {
@@ -10,29 +22,21 @@ function loadFromLS(key="empty") {
     } catch (error) {
          return data;
     }
-}
+};
 function restoreFromLS() {
     const data = loadFromLS(STORAGE_KEY) || {};
-    form.elements.email.value = data.email;
-    form.elements.message.value = data.input;
-}
-const STORAGE_KEY = 'feedback-form-state';
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', (e) => {
-    const userEmail = form.elements.email.value;
-    const userText = form.elements.text.value;
-    const data = {
-        email: userEmail,
-        input: userText
-    };
-    saveToLS(STORAGE_KEY, data);
-})
+   form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+};
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const data = loadFromLS(STORAGE_KEY) || {};
-console.log(data);
+    console.log(data);
 
-  localStorage.removeItem(STORAGE_KEY);
-  form.reset();
-})
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
+
+
