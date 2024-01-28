f6b3b10d22c59ea3f480b63f610101ab218458e6
Dima2430 <dima.volkkom@gmail.com>
2024-01-28 10:08:50 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,19 +2,19 @@ const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', (e) => {
     const userEmail = form.elements.email.value;
-    const userText = form.elements.text.value;
+    const userText = form.elements.message.value;
     const data = {
-        email: userEmail,
-        message: userText,
+        email: userEmail.trim(),
+        message: userText.trim(),
     };
     saveToLS(STORAGE_KEY, data);
 });
 
-function saveToLS(key = "empty", value = "") {
+function saveToLS(key, value = "") {
     const data = JSON.stringify(value);
     localStorage.setItem(key,data)
 };
-function loadFromLS(key="empty") {
+function loadFromLS(key) {
     const data = localStorage.getItem(key);
     try {
         const result = JSON.parse(data);
